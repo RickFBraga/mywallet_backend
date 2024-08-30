154f4dbcf937412eb8b926558a5fabb4d463167b
Henrique Braga <rickbprabelo@gmail.com>
2024-08-30 15:46:23 -0300

    chage server

--- src/server.js
+++ src/server.js
@@ -14,6 +14,10 @@ server.use(cors());
 server.use(authRouter);
 server.use(transactionsRouter);
 
+server.get("/", (req, res) => {
+  res.send("Express on render");
+});
+
 const port = process.env.PORT || 5000;
 
 server.listen(port, "0.0.0.0", () => {
